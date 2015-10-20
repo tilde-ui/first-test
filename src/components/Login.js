d3b33b623919222389c1aca8e2658d628a3f0f64
Ryan Herlihy <ryan.herlihy@sonian.net>
2015-10-20 10:14:25 -0400

    register button works, changed css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 class Login extends Component {
   render() {
     return (
-      <div className='col-md-4 col-md-offset-4'>
+      <div className='col-md-6 col-md-offset-3'>
         <div className='panel panel-default'>
           <div className='panel-heading lead'>Login</div>
           <div className='panel-body'>
@@ -14,17 +14,17 @@ class Login extends Component {
             <div className='form-group'>
               <input type='text' className='form-control' placeholder='Password' />
             </div>
-            <div className='form-group'>
+            <div>
               <Link to='/author'>
                 <button
-                  className='btn btn-primary col-md-6 col-md-offset-3'>
+                  className='btn btn-primary col-md-6'>
                   Login</button>
-                </Link>
-            </div>
-            <div className='form-group'>
-              <button
-                className='btn btn-default col-md-6 col-md-offset-3'>
-                Create New Account</button>
+              </Link>
+              <Link to='/register'>
+                <button
+                  className='btn btn-default col-md-6'>
+                  Create New Account</button>
+              </Link>
             </div>
           </div>
         </div>
